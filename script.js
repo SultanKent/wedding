20e98533d18cf71317f66be234c7e5c4c61d7125
Sultan <ffkirito6@gmail.com>
2023-09-01 22:25:16 +0600

    changed and it was improved

--- script.js
+++ script.js
@@ -1,133 +1,264 @@
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-analytics.js";
-  const firebaseConfig = {
-    apiKey: "AIzaSyAXPOZS9BqL_3K7_1WkTIit4KO5rwxb7uE",
-    authDomain: "wedding-edec2.firebaseapp.com",
-    databaseURL: "https://wedding-edec2-default-rtdb.firebaseio.com",
-    projectId: "wedding-edec2",
-    storageBucket: "wedding-edec2.appspot.com",
-    messagingSenderId: "101659396471",
-    appId: "1:101659396471:web:11e65008c36b656e60b972",
-    measurementId: "G-CY7KVVYYS0"
-  };
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
- 
-  const db = getDatabase();
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
+    import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-analytics.js";
+    import { getDatabase, ref, get, set } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-database.js";
+    
+    const firebaseConfig = {
+      apiKey: "AIzaSyAXPOZS9BqL_3K7_1WkTIit4KO5rwxb7uE",
+      authDomain: "wedding-edec2.firebaseapp.com",
+      databaseURL: "https://wedding-edec2-default-rtdb.firebaseio.com",
+      projectId: "wedding-edec2",
+      storageBucket: "wedding-edec2.appspot.com",
+      messagingSenderId: "101659396471",
+      appId: "1:101659396471:web:11e65008c36b656e60b972",
+      measurementId: "G-CY7KVVYYS0"
+    };
+    
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    const db = getDatabase();
+    const stageIndicators = document.querySelectorAll('.stage-indicator');
+    const backButton = document.createElement('button');
+    backButton.textContent = 'Назад';
+    const tableGrid = document.getElementById('table-grid');
+    
+    // Переменные для хранения текущего этапа и выбранных стола и сиденья
+    let currentStage = 1;
+    let selectedTable = null;
+    let selectedSeat = null;
 
-const tableGrid = document.getElementById('table-grid');
-const totalTables = 4;
-const seatsPerTable = 10;
-
-function updateSeatStatus(tableIndex, seatIndex, isOccupied, guestName) {
-  const seatRef = ref(db, `tables/${tableIndex}/seats/${seatIndex}`);
-  const newSeatData = {
-    isOccupied: isOccupied,
-    guestName: guestName
-  };
-
-  set(seatRef, newSeatData)
-    .then(() => {
-      renderTables(); // Обновление UI после обновления состояния
-    })
-    .catch(error => {
-      console.error('Error updating seat status:', error);
-    });
+    function updateStageDisplay() {
+  stageIndicators.forEach(indicator => {
+    const stage = parseInt(indicator.getAttribute('data-stage'));
+    if (stage === currentStage) {
+      indicator.classList.add('active');
+    } else {
+      indicator.classList.remove('active');
+    }
+  });
 }
 
-function renderTables() {
-  tableGrid.innerHTML = '';
-  for (let tableIndex = 0; tableIndex < totalTables; tableIndex++) {
-    const table = tables[tableIndex] || new Array(seatsPerTable).fill({ isOccupied: false, guestName: '' });
-    const seatRef = ref(db, `tables/${tableIndex}/seats`);
-    get(child(seatRef))
-      .then(snapshot => {
-        snapshot.forEach((seatSnapshot, seatIndex) => {
-          const seatData = seatSnapshot.val();
-          const seatElement = createSeatElement(tableIndex, seatIndex, seatData);
+    
+    function updateSeatStatus(tableIndex, seatIndex, isOccupied, guestName) {
+      const seatRef = ref(db, `tables/${tableIndex}/seats/${seatIndex}`);
+      const newSeatData = {
+        isOccupied: isOccupied,
+        guestName: guestName
+      };
+    
+      set(seatRef, newSeatData)
+        .then(() => {
+          if (currentStage === 1) {
+            showSeats();
+          } else if (currentStage === 2) {
+            showUserInfoForm();
+          }
+        })
+        .catch(error => {
+          console.error('Error updating seat status:', error);
         });
-      })
-      .catch(error => {
-        console.error('Error fetching seat data:', error);
-      });
-    const tableElement = document.createElement('div');
-    tableElement.className = 'table';
-
-    const rowContainer = document.createElement('div');
-    rowContainer.className = 'seat-row-container';
-
-    const topRow = document.createElement('div');
-    topRow.className = 'seat-row';
-    for (let seatIndex = 0; seatIndex < seatsPerTable / 2; seatIndex++) {
-      const seat = table[seatIndex];
-
-      const seatElement = createSeatElement(tableIndex, seatIndex, seat);
-      topRow.appendChild(seatElement);
     }
-    rowContainer.appendChild(topRow);
-
-    const centerDiv = document.createElement('div');
-    centerDiv.className = 'center-div';
-
-    rowContainer.appendChild(centerDiv);
-
-    const bottomRow = document.createElement('div');
-    bottomRow.className = 'seat-row';
-    for (let seatIndex = seatsPerTable / 2; seatIndex < seatsPerTable; seatIndex++) {
-      const seat = table[seatIndex];
-
-      const seatElement = createSeatElement(tableIndex, seatIndex, seat);
-      bottomRow.appendChild(seatElement);
+    
+    function showTables() {
+      currentStage = 1;
+      tableGrid.innerHTML = '';
+      const totalTables = 4;
+      for (let tableIndex = 0; tableIndex < totalTables; tableIndex++) {
+        const tableElement = document.createElement('div');
+        tableElement.className = 'table';
+        tableElement.textContent = `Стол ${tableIndex + 1}`;
+        tableElement.addEventListener('click', () => {
+          selectedTable = tableIndex;
+          showSeats();
+        });
+        tableGrid.appendChild(tableElement);
+        updateStageDisplay();
+      }
     }
-    rowContainer.appendChild(bottomRow);
+    
+  function showSeats() {
+  currentStage = 2;
+  tableGrid.innerHTML = '';
 
-    tableElement.appendChild(rowContainer);
+  // Создаем div с классом 'seats-content'
+  const seatsContent = document.createElement('div');
+  seatsContent.className = 'seats-content';
 
-    tableGrid.appendChild(tableElement);
-  }
+  // Создаем два контейнера для сиденьй
+  const seatsContainer1 = document.createElement('div');
+  seatsContainer1.className = 'seats';
+  const seatsContainer2 = document.createElement('div');
+  seatsContainer2.className = 'seats';
+
+  const seatsPerTable = 10;
+  const tableRef = ref(db, `tables/${selectedTable}`);
+  get(tableRef).then(snapshot => {
+    const tableData = snapshot.val();
+    if (tableData && tableData.seats) {
+      // Разделяем сиденья на два контейнера (по 5 сиденьй в каждом)
+      for (let seatIndex = 0; seatIndex < seatsPerTable; seatIndex++) {
+        const seatElement = document.createElement('div');
+        seatElement.className = 'seat';
+        seatElement.textContent = `Сиденье ${seatIndex + 1}`;
+        
+        if (seatIndex < 5) {
+          seatsContainer1.appendChild(seatElement);
+        } else {
+          seatsContainer2.appendChild(seatElement);
+        }
+
+        if (!tableData.seats[seatIndex].isOccupied) {
+          seatElement.addEventListener('click', () => {
+            selectedSeat = seatIndex;
+            showUserInfoForm();
+          });
+        } else {
+          seatElement.classList.add('occupied');
+          seatElement.textContent = tableData.seats[seatIndex].guestName;
+        }
+      }
+      seatsContent.appendChild(seatsContainer1);
+      seatsContent.appendChild(seatsContainer2);
+
+      // Добавляем seatsContent в tableGrid
+      tableGrid.appendChild(seatsContent);
+
+      backButton.removeEventListener('click', showSeats);
+      backButton.removeEventListener('click', showUserInfoForm);
+      backButton.addEventListener('click', showTables); // Возврат к этапу 1
+      tableGrid.appendChild(backButton);
+      updateStageDisplay();
+    }
+  });
 }
 
-function createSeatElement(tableIndex, seatIndex, seat) {
-  const seatElement = document.createElement('div');
-  seatElement.className = 'seat';
+    
+function showUserInfoForm() {
+  currentStage = 3;
+  tableGrid.innerHTML = '';
 
-  if (seat.isOccupied) {
-    seatElement.classList.add('occupied');
-    seatElement.textContent = seat.guestName;
-  } else {
-    seatElement.classList.add('available');
-    seatElement.addEventListener('click', () => {
-      const modal = document.createElement('div');
-      modal.className = 'modal';
+  const formContainer = document.createElement('div');
+  formContainer.className = 'form-container';
 
-      const input = document.createElement('input');
-      input.type = 'text';
-      input.placeholder = 'Enter guest name';
-      modal.appendChild(input);
+  const nameLabel = document.createElement('label');
+  nameLabel.textContent = 'Имя:';
+  const nameInput = document.createElement('input');
+  nameInput.type = 'text';
+  
+  const genderTitle = document.createElement('p');
+  genderTitle.textContent = 'Ваш пол';
+  const genderContainer = document.createElement('div'); // Создаем контейнер для пола
+  genderContainer.id = 'gender';
+  // Первый вложенный div для инпута и метки М
+  const maleDiv = document.createElement('div');
+  const maleCheckbox = document.createElement('input');
+  maleCheckbox.type = 'checkbox';
+  maleCheckbox.name = 'gender';
+  maleCheckbox.value = 'male';
+  maleCheckbox.id = 'maleCheckbox';
+  const maleLabel = document.createElement('label');
+  maleLabel.textContent = 'М';
+  maleLabel.htmlFor = 'maleCheckbox';
+  // Второй вложенный div для инпута и метки Ж
+  const femaleDiv = document.createElement('div');
+  const femaleCheckbox = document.createElement('input');
+  femaleCheckbox.type = 'checkbox';
+  femaleCheckbox.name = 'gender';
+  femaleCheckbox.value = 'female';
+  femaleCheckbox.id = 'femaleCheckbox';
+  const femaleLabel = document.createElement('label');
+  femaleLabel.textContent = 'Ж';
+  femaleLabel.htmlFor = 'femaleCheckbox';
 
-      const addButton = document.createElement('button');
-      addButton.textContent = 'Add';
-      addButton.addEventListener('click', () => {
-        const guestName = input.value;
-        if (guestName) {
-          updateSeatStatus(tableIndex, seatIndex, true, guestName);
-          document.body.removeChild(modal);
+  maleDiv.appendChild(maleCheckbox);
+  maleDiv.appendChild(maleLabel);
+  femaleDiv.appendChild(femaleCheckbox);
+  femaleDiv.appendChild(femaleLabel);
+  genderContainer.appendChild(maleDiv);
+  genderContainer.appendChild(femaleDiv);
+
+  // Создаем счетчик возраста, как и ранее
+  const ageLabel = document.createElement('label');
+  ageLabel.textContent = 'Возраст:';
+  const ageCounter = document.createElement('div');
+  ageCounter.className = 'age-counter';
+  const ageDisplay = document.createElement('div');
+  ageDisplay.className = 'age-display';
+  ageDisplay.textContent = '0';
+  // Добавляем кнопки для увеличения и уменьшения возраста
+  const incrementButton = document.createElement('button');
+  incrementButton.textContent = '+';
+  incrementButton.addEventListener('click', () => {
+    const currentAge = parseInt(ageDisplay.textContent);
+    if (!isNaN(currentAge)) {
+      ageDisplay.textContent = (currentAge + 1).toString();
+    }
+  });
+
+  const decrementButton = document.createElement('button');
+  decrementButton.textContent = '-';
+  decrementButton.addEventListener('click', () => {
+    const currentAge = parseInt(ageDisplay.textContent);
+    if (!isNaN(currentAge) && currentAge > 0) {
+      ageDisplay.textContent = (currentAge - 1).toString();
+    }
+  });
+  ageCounter.appendChild(decrementButton);
+  ageCounter.appendChild(ageDisplay);
+  ageCounter.appendChild(incrementButton);
+
+  formContainer.appendChild(nameLabel);
+  formContainer.appendChild(nameInput);
+
+  formContainer.appendChild(genderTitle);
+  formContainer.appendChild(genderContainer);
+  formContainer.appendChild(ageLabel);
+  formContainer.appendChild(ageCounter);
+
+  const submitButton = document.createElement('button');
+  submitButton.textContent = 'Подтвердить';
+  submitButton.addEventListener('click', () => {
+    const name = nameInput.value;
+    const gender = maleCheckbox.checked ? 'М' : (femaleCheckbox.checked ? 'Ж' : 'Не выбран');
+    const age = ageDisplay.textContent;
+    if (name && gender !== 'Не выбран' && age >= 0) {
+      submitUserInfo(name, gender, age);
+      tableGrid.innerHTML = ''; // Очистка содержимого tableGrid при отправке данных
+    }
+  });
+
+  formContainer.appendChild(submitButton);
+
+  // Добавление formContainer в tableGrid
+  tableGrid.appendChild(formContainer);
+
+  backButton.addEventListener('click', showSeats);
+  tableGrid.appendChild(backButton);
+  updateStageDisplay();
+}
+
+
+
+    
+    function submitUserInfo(name, gender) {
+      // Здесь вы можете записать информацию о пользователе, столе и месте в базу данных
+      // Или отобразить эту информацию пользователю
+      const tableRef = ref(db, `tables/${selectedTable}`);
+      get(tableRef).then(snapshot => {
+        const tableData = snapshot.val();
+        if (tableData && tableData.seats && tableData.seats[selectedSeat]) {
+          tableData.seats[selectedSeat].isOccupied = true;
+          tableData.seats[selectedSeat].guestName = name;
+          set(tableRef, tableData)
+            .then(() => {
+              const message = `Вы выбрали стол ${selectedTable + 1}, сиденье ${selectedSeat + 1}. Имя: ${name}, Пол: ${gender}`;
+              alert(message);
+              showTables();
+            })
+            .catch(error => {
+              console.error('Error updating table data:', error);
+            });
         }
       });
-      modal.appendChild(addButton);
-      
-      const cancelButton = document.createElement('button');
-      cancelButton.textContent = 'Cancel';
-      cancelButton.addEventListener('click', () => {
-        document.body.removeChild(modal);
-      });
-      modal.appendChild(cancelButton);
-
-      document.body.appendChild(modal);
-    });
-  }
-
-  return seatElement;
-}
-
-renderTables();
+    }
+    
+    showTables();
